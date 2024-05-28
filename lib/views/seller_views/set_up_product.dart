af578a609a308f5e4fd7a7173e6f2bf3e6187149
saroj-kumar-padhi <surajpadhi01@gmail.com>
2024-05-28 08:26:51 +0530

    courasal

--- lib/views/seller_views/set_up_product.dart
+++ lib/views/seller_views/set_up_product.dart
@@ -1,7 +1,11 @@
+import 'dart:io';
+
+import 'package:carousel_slider/carousel_slider.dart';
 import 'package:dekhlo/controllers/productSetupController.dart';
 import 'package:dekhlo/utils/components/coustoumTextField.dart';
 import 'package:dekhlo/utils/components/textstyle.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:flutter_svg/flutter_svg.dart';
 import 'package:flutter_svg/svg.dart';
@@ -23,8 +27,9 @@ class SetUpProduct extends StatelessWidget {
   final ProductSetUpController productSetUpController =
       Get.put(ProductSetUpController());
 
-  RxString imagePath = ''.obs;
-
+  final RxList<String> imagePaths = <String>[].obs;
+  RxBool isLiked = false.obs;
+  RxInt currentPage = 0.obs;
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -58,60 +63,148 @@ class SetUpProduct extends StatelessWidget {
               SizedBox(
                 height: 20.h,
               ),
-              InkWell(
-                onTap: () async {
-                  final result = await showDialog<String>(
-                    context: context,
-                    builder: (BuildContext context) {
-                      return const PickImageDialog();
-                    },
-                  );
-                  if (result != null) {
-                    imagePath.value = result;
-                  }
-                },
-                child: Center(
-                  child: Container(
-                    height: 100.h,
-                    width: 150.w,
-                    decoration: BoxDecoration(
-                      border: Border.all(
-                        color: const Color(0xffC4C4C4), // Border color
-                        width: 1.0.w, // Border width
-                      ),
-                      borderRadius:
-                          BorderRadius.circular(10.0.r), // Border radius
-                    ),
-                    child: Padding(
-                      padding: EdgeInsets.all(
-                          MediaQuery.of(context).size.height * 0.01),
-                      child: Column(
+              Obx(() {
+                return imagePaths.value.isEmpty
+                    ? addImages(context)
+                    : Column(
                         children: [
+                          CarouselSlider.builder(
+                            options: CarouselOptions(
+                              enableInfiniteScroll: false,
+                              // enlargeCenterPage: true,
+                              onPageChanged: (index, reason) {
+                                currentPage.value = index;
+                              },
+                              viewportFraction: 0.4,
+                              height: 160.0.h,
+                            ),
+                            itemCount: imagePaths.length,
+                            itemBuilder: (BuildContext context, int index,
+                                int realIndex) {
+                              final String imagePath = imagePaths[index];
+                              return Column(
+                                children: [
+                                  Stack(
+                                    children: [
+                                      Container(
+                                        decoration: BoxDecoration(
+                                            borderRadius:
+                                                BorderRadius.circular(4.r),
+                                            border: Border.all(
+                                                width: 1,
+                                                color:
+                                                    const Color(0xff828282))),
+                                        child: Padding(
+                                          padding: EdgeInsets.fromLTRB(
+                                              8.0.w, 12.h, 8.w, 12.h),
+                                          child: Container(
+                                            height: 100,
+                                            width: 100,
+                                            margin: const EdgeInsets.symmetric(
+                                                horizontal: 5.0),
+                                            decoration: const BoxDecoration(
+                                                color: Colors.amber),
+                                            child: Image.file(File(imagePath),
+                                                fit: BoxFit.cover),
+                                          ),
+                                        ),
+                                      ),
+                                      Obx(() => Positioned(
+                                              child: Row(
+                                            children: [
+                                              isLiked.value
+                                                  ? InkWell(
+                                                      onTap: () {
+                                                        isLiked.value =
+                                                            !isLiked.value;
+                                                      },
+                                                      child: const Icon(
+                                                        Icons.star,
+                                                        color:
+                                                            Color(0xffFFD361),
+                                                      ),
+                                                    )
+                                                  : InkWell(
+                                                      onTap: () {
+                                                        isLiked.value =
+                                                            !isLiked.value;
+                                                      },
+                                                      child: const Icon(
+                                                        Icons.star_border,
+                                                        color: Colors.grey,
+                                                      ),
+                                                    ),
+                                              SizedBox(
+                                                width: 80.w,
+                                              ),
+                                              InkWell(
+                                                onTap: () {
+                                                  imagePaths.remove(imagePath);
+                                                },
+                                                child: const Icon(
+                                                  Icons.cancel,
+                                                  color: Color(0xff4A4A4A),
+                                                ),
+                                              )
+                                            ],
+                                          )))
+                                    ],
+                                  ),
+                                  SizedBox(
+                                    height: 10.h,
+                                  ),
+                                  imagePaths.value.length > 1
+                                      ? showIndicator()
+                                      : const SizedBox(),
+                                ],
+                              );
+                            },
+                          ),
                           SizedBox(
-                            height: 20.h,
+                            height: 10.h,
                           ),
-                          SvgPicture.asset(
-                            "assest/camera_orange.svg",
-                            height: 30.h,
-                            width: 50.w, // Corrected spelling of "assets"
-                            // Adjust height as needed
+                          GestureDetector(
+                            onTap: () async {
+                              final result = await showDialog<String>(
+                                context: context,
+                                builder: (BuildContext context) {
+                                  return const PickImageDialog();
+                                },
+                              );
+                              if (result != null) {
+                                imagePaths.add(result);
+                              }
+                            },
+                            child: Container(
+                              decoration: BoxDecoration(
+                                  borderRadius: BorderRadius.circular(4.r),
+                                  border: Border.all(
+                                      width: 1,
+                                      color: const Color(0xffFC8019))),
+                              child: Padding(
+                                padding: const EdgeInsets.all(8.0),
+                                child: Row(
+                                  mainAxisSize: MainAxisSize.min,
+                                  children: [
+                                    const Center(
+                                      child: Icon(
+                                        Icons.add,
+                                        color: Color(0xffFC8019),
+                                      ),
+                                    ),
+                                    Text("Add images",
+                                        style: TextStyles.openSans(
+                                            fontSize: 16.sp,
+                                            fontWeight: FontWeight.w600,
+                                            color: const Color(0xffFC8019)))
+                                  ],
+                                ),
+                              ),
+                            ),
                           ),
-                          SizedBox(
-                            height: 5.h,
-                          ),
-                          Text(
-                            "Add your store images",
-                            style: TextStyles.openSans(
-                                fontWeight: FontWeight.w400,
-                                fontSize: 10.sp,
-                                color: const Color(0xffFC8019)),
-                          )
                         ],
-                      ),
-                    ),
-                  ),
-                ),
-              ),
+                      );
+              }),
               SizedBox(
                 height: 10.h,
               ),
@@ -531,6 +624,61 @@ class SetUpProduct extends StatelessWidget {
     );
   }
 
+  InkWell addImages(BuildContext context) {
+    return InkWell(
+      onTap: () async {
+        final result = await showDialog<String>(
+          context: context,
+          builder: (BuildContext context) {
+            return const PickImageDialog();
+          },
+        );
+        if (result != null) {
+          imagePaths.add(result);
+        }
+      },
+      child: Center(
+        child: Container(
+          height: 100.h,
+          width: 150.w,
+          decoration: BoxDecoration(
+            border: Border.all(
+              color: const Color(0xffC4C4C4), // Border color
+              width: 1.0.w, // Border width
+            ),
+            borderRadius: BorderRadius.circular(10.0.r), // Border radius
+          ),
+          child: Padding(
+            padding: EdgeInsets.all(MediaQuery.of(context).size.height * 0.01),
+            child: Column(
+              children: [
+                SizedBox(
+                  height: 20.h,
+                ),
+                SvgPicture.asset(
+                  "assest/camera_orange.svg",
+                  height: 30.h,
+                  width: 50.w, // Corrected spelling of "assets"
+                  // Adjust height as needed
+                ),
+                SizedBox(
+                  height: 5.h,
+                ),
+                Text(
+                  "Add your store images",
+                  style: TextStyles.openSans(
+                      fontWeight: FontWeight.w400,
+                      fontSize: 10.sp,
+                      color: const Color(0xffFC8019)),
+                )
+              ],
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+
   Container timeEditingBox({required TextEditingController controller}) {
     return Container(
       height: 48.h,
@@ -601,6 +749,29 @@ class SetUpProduct extends StatelessWidget {
           fontSize: 14.sp,
           fontWeight: FontWeight.w600,
           color: const Color(0xff313333)),
+    );
+  }
+
+  showIndicator() {
+    return Row(
+      mainAxisSize: MainAxisSize.min,
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: List.generate(
+        imagePaths.length,
+        (index) => Padding(
+          padding: const EdgeInsets.symmetric(horizontal: 4.0),
+          child: Obx(
+            () => Container(
+              height: 8.h,
+              width: 8.w,
+              decoration: BoxDecoration(
+                shape: BoxShape.circle,
+                color: index == currentPage.value ? Colors.black : Colors.grey,
+              ),
+            ),
+          ),
+        ),
+      ),
     );
   }
 }
